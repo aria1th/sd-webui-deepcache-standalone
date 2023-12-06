2fbc3788de42530442af8992a2465817a85871bf
AngelBottomless <aria1th@naver.com>
2023-12-06 10:54:03 +0900

    Quick fix: handle possible SGM class
    If some reallllly weird extension was crearing child classes from SGM's class, we have to handle it...

--- scripts/forward_timestep_embed_patch.py
+++ scripts/forward_timestep_embed_patch.py
@@ -12,19 +12,32 @@ try:
     from ldm.modules.diffusionmodules.openaimodel import VideoResBlock
 except (ImportError, ModuleNotFoundError):
     VideoResBlock = None
+
+# SD XL modules from generative-models repo
+from sgm.modules.attention import SpatialTransformer as SpatialTransformerSGM
+try:
+    from sgm.modules.attention import SpatialVideoTransformer as SpatialVideoTransformerSGM
+except (ImportError, ModuleNotFoundError):
+    SpatialVideoTransformerSGM = None
+from sgm.modules.diffusionmodules.openaimodel import TimestepBlock as TimestepBlockSGM, Upsample as UpsampleSGM
+try:
+    from sgm.modules.diffusionmodules.openaimodel import VideoResBlock as VideoResBlockSGM
+except (ImportError, ModuleNotFoundError):
+    VideoResBlockSGM = None
+
 import torch.nn.functional as F
 
 def forward_timestep_embed(ts:TimestepEmbedSequential, x, emb, context=None, output_shape=None, time_context=None, num_video_frames=None, image_only_indicator=None):
     for layer in ts:
-        if VideoResBlock and isinstance(layer, VideoResBlock):
+        if VideoResBlock and isinstance(layer, (VideoResBlock, VideoResBlockSGM)):
             x = layer(x, emb, num_video_frames, image_only_indicator)
-        elif isinstance(layer, TimestepBlock):
+        elif isinstance(layer, (TimestepBlock, TimestepBlockSGM)):
             x = layer(x, emb)
-        elif SpatialVideoTransformer and isinstance(layer, SpatialVideoTransformer):
+        elif SpatialVideoTransformer and isinstance(layer, (SpatialVideoTransformer, SpatialVideoTransformerSGM)):
             x = layer(x, context, time_context, num_video_frames, image_only_indicator)
-        elif isinstance(layer, SpatialTransformer):
+        elif isinstance(layer, (SpatialTransformer, SpatialTransformerSGM)):
             x = layer(x, context)
-        elif isinstance(layer, Upsample):
+        elif isinstance(layer, (Upsample, UpsampleSGM)):
             x = forward_upsample(layer, x, output_shape=output_shape)
         else:
             x = layer(x)
